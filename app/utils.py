8e07ae1c91392e3049467ee58092fdb1fafe2a3d
xirao <2642179769@qq.com>
2025-02-17 14:49:30 +0800

    修改时区错误问题

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timezone, timedelta
 from .config import settings
 
+
 def get_china_time():
-    utc_now = datetime.now(timezone.utc)  # 获取当前 UTC 时间
-    china_time = utc_now + timedelta(hours=settings.CHINA_TIMEZONE_OFFSET)  # 加上时区偏移
+    # 创建一个中国时区的对象 (UTC+8)
+    china_tz = timezone(settings.CHINA_TIMEZONE_OFFSET)  # 中国时区为 UTC+8
+    # 获取当前的 UTC 时间，并转换为中国时区时间
+    china_time = datetime.now(china_tz)
     return china_time
